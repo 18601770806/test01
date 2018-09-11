66c9a6e6fdff51c5f89ad8277be0c6be0b0d86a3
18601770806 <18601770806@163.com>
2018-09-11 14:47:10 +0800

    a

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,5 +11,6 @@ public class DemoApplication {
 		System.out.println("start.....");
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("end.....a");
+        System.out.println("end.....b");
 	}
 }
